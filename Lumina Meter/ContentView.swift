5bbaabd463ba19fb5f565544ab591c6492d68952
Kevin Lin <kevin.lin29@outlook.com>
2026-02-07 01:35:29 -0500

    Initial Commit

---
+++ Lumina Meter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lumina Meter
+//
+//  Created by Kevin Lin on 2026-02-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lumina Meter/Lumina_MeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lumina_MeterApp.swift
+//  Lumina Meter
+//
+//  Created by Kevin Lin on 2026-02-07.
+//
+
+import SwiftUI
+
+@main
+struct Lumina_MeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
